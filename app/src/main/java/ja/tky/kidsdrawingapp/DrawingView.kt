00f32416dd1de4e0b159703825189008a65f3cb0
tky <takayamac@icloud.com>
2022-01-10 16:52:31 +0900

    KtファイルからViewを表示して絵を描画できる状態を作成

--- app/src/main/java/ja/tky/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/ja/tky/kidsdrawingapp/DrawingView.kt
@@ -1,8 +1,10 @@
+// このファイルが画面のViewになる
 package ja.tky.kidsdrawingapp
 
 import android.content.Context
 import android.graphics.* // .* することでgraphicsライブラリ全てがimportされる
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 // ①
@@ -28,6 +30,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
     // 初期値の設定関数
     private fun setUpDrawing() {
+        // インナークラス(CustomPath)に引数を渡してインスタンスを作成する
         mDrawPath = CustomPath(color, mBrushSize)
         // ここでペイントオブジェクトを代入
         mDrawPaint = Paint()
@@ -39,11 +42,86 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mBrushSize = 20.toFloat()
     }
 
+    // ⑤　https://developer.android.com/reference/android/view/View#size,-padding-and-margins
+    // onSizeChangedは元々Viewに用意されているメソッド
+    // Viewのサイズが変更されたときに呼び出される
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        // ビットマップ作成
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        // キャンバスにビットマップを渡す。(nullを渡せないので「!!」を付けてnullの場合に例外となるように引数に渡す)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    // ⑥　https://developer.android.com/reference/android/view/View#size,-padding-and-margins
+    // onDrawは元々Viewに用意されているメソッド
+    // Viewに描きたい内容をonDrawで記述する
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        // drawBitmapを使って描画する
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        // mDrawPathが存在しない場合
+        if (!mDrawPath!!.isEmpty) {
+            // ペイントの幅を取得
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            // ペイントの色を取得
+            mDrawPaint!!.color = mDrawPath!!.color
+            // パスを渡して描画する
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    // ⑦
+    // onTouchEventは元々Viewに用意されているメソッド
+    // 実際に描画するイベント
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // 座標を取得
+        val touchX = event.x
+        val touchY = event.y
+
+        // ⑧ 各アクションが発生した時のアクションを設定
+        when (event.action) {
+            // 画面タップ時
+            MotionEvent.ACTION_DOWN -> {
+                println("タップした")
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                // パスのリセット
+                mDrawPath!!.reset()
+                // moveToで引数のxとy座標に目掛けて描き始めますよ。という命令を送る
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+
+            // 動かした時
+            MotionEvent.ACTION_MOVE -> {
+                // lineToで引数のxとyに線を引きますよ。という命令を送る
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+
+            // タッチ状態から手を離した時
+            MotionEvent.ACTION_UP -> {
+                // パスを上書き（現状では描いたパスが消えてしまう）
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+
+            else -> return false
+        }
+
+        // invalidate()を呼び出すことでonDrawが再度呼び出される
+        // onDrawが呼ばれることで再描画される
+        invalidate()
+
+        // trueを返すことで何回も処理を繰り返すことができる
+        return true
+    }
+
     // ③
     // DrawingView内でのみ使用できるCustomPathクラス
     // graphicsライブラリのPathをimport
     // CustomPathクラスは色と、ブラシの太さを持つ(colorとbrushThickness)
-    internal inner class CustomPath(var color: Int, brushThickness: Float) : Path() {
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
     }
 
