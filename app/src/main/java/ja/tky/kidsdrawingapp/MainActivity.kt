0c8749402f04588a301605796bc9e2f8940317ae
tky <takayamac@icloud.com>
2022-01-15 22:51:16 +0900

    ブラシサイズ変更のダイアログ表示と、選択でサイズ変更の実装

--- app/src/main/java/ja/tky/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/ja/tky/kidsdrawingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package ja.tky.kidsdrawingapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +21,59 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
 
+        // ブラシボタン
+        val ibBrash: ImageButton = findViewById(R.id.ibBrush)
+        ibBrash.setOnClickListener {
+            showBrushSizeChooseDialog()
+        }
+    }
+
+    /**
+     * ブラシダイアログを表示する関数
+     * **/
+    private fun showBrushSizeChooseDialog() {
+        // ダイアログ インスタンス生成
+        val brushDialog = Dialog(this)
+
+        // ----- ダイアログ生成 start -----------
+
+        // 表示するレイアウトを設定
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        // タイトル設定
+        brushDialog.setTitle("ブラシサイズ")
+        // 設定したレイアウトからボタン情報を取得
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ibSmallBrush)
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ibMediumBrush)
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ibLargeBrush)
+
+        // 最小のアイコンタップした時のアクション
+        smallBtn.setOnClickListener {
+            // drawingView.ktで設定している関数を呼び出す(対象のブラシサイズを設定)
+            drawingView?.setSizeForBrush(10.toFloat())
+            // ダイアログを閉じる
+            brushDialog.dismiss()
+        }
+
+        // 真ん中のアイコンタップした時のアクション
+        mediumBtn.setOnClickListener {
+            // drawingView.ktで設定している関数を呼び出す(対象のブラシサイズを設定)
+            drawingView?.setSizeForBrush(20.toFloat())
+            // ダイアログを閉じる
+            brushDialog.dismiss()
+        }
+
+        // 最大のアイコンタップした時のアクション
+        largeBtn.setOnClickListener {
+            // drawingView.ktで設定している関数を呼び出す(対象のブラシサイズを設定)
+            drawingView?.setSizeForBrush(30.toFloat())
+            // ダイアログを閉じる
+            brushDialog.dismiss()
+        }
+
+        // 閉じるアクションが発火した時はスルーされる
+        // ダイアログを表示
+        brushDialog.show()
+
+        // ------ ダイアログ生成完了 -------------
     }
 }
